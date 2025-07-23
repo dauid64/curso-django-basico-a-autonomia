47f34ba2740b4bfaae3c7ec33c9f248a8d7ffa35
dauid64 <carlosdauid64br@gmail.com>
2025-07-23 11:57:39 -0300

    aula 08

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,29 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+PROJETOS = [
+    {
+        "titulo": "Projeto 1",
+        "descricao": "Descrição do Projeto 1",
+        "link": "#",
+        "imagem": "/static/blog/img/projeto.png"
+    },
+    {
+        "titulo": "Projeto 2",
+        "descricao": "Descrição do Projeto 2",
+        "link": "#",
+        "imagem": "/static/blog/img/projeto.png"
+    },
+    {
+        "titulo": "Projeto 3",
+        "descricao": "Descrição do Projeto 3",
+        "link": "#",
+        "imagem": "/static/blog/img/projeto.png"
+    }
+]
+
 def index(request):
-    return render(request, 'blog/index.html')
+    title_page = "Página principal"
+    return render(request, 'blog/index.html', {
+        "title_page": title_page,
+        "projetos": PROJETOS
+    })
