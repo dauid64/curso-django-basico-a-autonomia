52249c6605ae5c92815380434e317939a3f4957f
dauid64 <carlosdauid64br@gmail.com>
2025-07-29 00:29:52 -0300

    Aula 09

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    content = models.TextField()
+    active = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to="projects/")
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': '5433',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'NAME': 'blog_db',
     }
 }
 
